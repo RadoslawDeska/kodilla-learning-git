e17a4e4bf26098e6bcb1c2cd50ef69368968bd81
RadoslawDeska <69204738+RadoslawDeska@users.noreply.github.com>
2025-10-16 11:22:37 +0200

    Add the for loop

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,11 @@ shopping_list = {
     "Piekarnia": ["chleb", "bułki", "pączek"],
     "Warzywniak": ["marchew", "seler", "rukola"],
 }
+
+cnt = 0
+
+print("Lista zakupów:")
+for store in shopping_list:
+    print(f"Idę do {store} i kupuję tam: {shopping_list[store]}.")
+    cnt += len(shopping_list[store])
+print(f"W sumie kupuję {cnt} produktów.")
